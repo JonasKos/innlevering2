0742a22cfa6e28e6c0375f21f4015b8a98314758
Jonas Kosmo <jonaskos@stud.ntnu.no>
2023-02-16 18:30:42 +0100

    awd

--- src/views/SimpleForm.vue
+++ src/views/SimpleForm.vue
@@ -4,7 +4,7 @@
     <form @submit.prevent="sendForm">
       <h3>Leave feedback for the calculator</h3>
         <div><BaseInput v-model="feedback.name" label="Name" type="text" /></div>
-        <div><BaseInput v-model="feedback.email" label="Email" type="email" /></div>
+        <div><BaseInput v-model="feedback.email" label="Email" type="email"/></div>
         <div><BaseInput v-model="feedback.message" label="Message" type="text" /></div>
       
       
@@ -17,13 +17,16 @@
 <script>
 import BaseInput from "../components/BaseInput.vue";
 import axios from 'axios';
+//import { useField, useForm } from "vee-validate";
 export default {
     
   name: "SimpleForm",
   components: {
     BaseInput,
   },
+
   data() {
+
     return {
       feedback: {
         name: "",
@@ -34,8 +37,17 @@ export default {
   },
     methods: {
         sendForm() {
-            //handle form submission
+            axios.post('https://my-json-server.typicode.com/JonasKos/innlevering2/feedback', this.feedback)
+            .then(function(response){
+                alert('Success: ' + response);
+                console.log(response);
+            })
+            .catch(function(err){
+                alert('Error: ' + err);
+                console.log('error', err);
+            });
         },
+
     },
 };
 </script>
